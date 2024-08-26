f38d97282628b4018e3ba9ea113d3432cddfd6b3
pranta <prantanathnayan@gmail.com>
2024-08-27 00:42:27 +0600

    made responsive version

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   gradientColorClass = "bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent";
+  isMenuOpen = false;
 }
